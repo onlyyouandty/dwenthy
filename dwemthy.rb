60b274606e973b99ef039daf7c5d5191446406f0
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-09-16 18:50:28 +0100

    add hit/fight method and rabbit file

--- dwemthy.rb
+++ dwemthy.rb
@@ -36,11 +36,55 @@ end
 
 # Creature atrributes read-only
 traits :life, :strength, :chrisma, :weapon
+
+# This method applies a hit during a fight
+	def hit (damage)
+	p_up = rand(charisma)
+	if p_up % 9 == 7
+		@life += p_up / 4
+		puts "[#{self.class} magick powers up #{p_up}!]"
+	end
+	@life -= damage
+	puts "[#{self.class} has died.]" if @life <= 0
+	end
+
+	#This method takes one turn in a fight.
+	def fight( enemy, weapon )
+		if life <= 0
+			puts "[#{ self.class } is too dead to fight!]"
+			return
+		end
+
+		#Attack the opponet
+		your_hit = rand(strength + weapon)
+		puts "[You hit with #{ your_hit } points of damage!]"
+		enemy.hit( your_hit )
+
+		# Retaliation
+		p enemy
+		if enemy.life > 0
+			enemy_hit = rand( enemy.strength + enemy.weapon )
+			puts "[Your enemy hit with #{ enemy_hit } points of damage]"
+			self.hit ( enemy_hit )
+		end
+	end
+
+
 end
 
-# class Dragon < Creature
-# 	life 1340		# tough scales
-# 	strength 451 	# bristling veins
-# 	charisma 1020	# toothy smile
-# 	weapon 939 		#fire breath
-# end
+class DwemthysArray < Array
+	alias _inspect inspect
+	def inspect; "#<#{ self.class }#{ _inspect }>"; end
+	def method_missing ( meth, *args )
+		answer = first.send( meth, *args )
+		if first.life <= 0
+			shift
+			if empty?
+				puts "[Whoa. You decimated Dwemthy's Array!]"
+			else
+				puts "[Get ready #{first.class} has emerged.]"
+			end
+		end
+		answer || 0
+	end
+end
---
+++ rabbit.rb
@@ -0,0 +1,2 @@
+class Rabbit < Creature
+	traits : bomb
